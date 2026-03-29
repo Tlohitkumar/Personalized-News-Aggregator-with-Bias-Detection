6ceb03892863728852571d21fb509d8c186fc87f
Tlohitkumar <lohitkumarthotakura@gmail.com>
2026-03-29 16:55:10 +0530

    Addeed the getMapping to check through API to retriving the User data from the repository

--- src/main/java/com/example/news/controller/UserController.java
+++ src/main/java/com/example/news/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.example.news.controller;
 
+import java.util.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +25,10 @@ public class UserController {
 		@PostMapping("/register")
 		public User register(@RequestBody User user) {
 			return userService.registerUser(user);
+		}
+		
+		@GetMapping
+		public List<User> getAllUsers() {
+		    return userService.getAllUsers();
 		}
 }
