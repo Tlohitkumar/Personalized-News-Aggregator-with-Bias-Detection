e4d8e358776e5caf512b1a35d65abb7646ce3abf
Tlohitkumar <lohitkumarthotakura@gmail.com>
2026-03-30 13:08:13 +0530

    Updtated the serice laer by adding deleteByUserId() to Delete the user with id

--- src/main/java/com/example/news/service/UserService.java
+++ src/main/java/com/example/news/service/UserService.java
@@ -18,5 +18,13 @@ public class UserService {
 	public List<User> getAllUsers() {
 	    return userRepository.findAll();
 	}
+	
+	public User getUserById(Long id) {
+	    return userRepository.findById(id).orElse(null);
+	}
+
+	public void deleteUser(Long id) {
+	    userRepository.deleteById(id);
+	}
 }
 
