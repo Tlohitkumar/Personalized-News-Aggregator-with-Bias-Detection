0c43e313587df5e9ee20fddaf239556e6af1b07e
Tlohitkumar <lohitkumarthotakura@gmail.com>
2026-03-30 13:06:32 +0530

    Updated the Controller with getMapping to and Delete Mapping to check working of Data Base

--- src/main/java/com/example/news/controller/UserController.java
+++ src/main/java/com/example/news/controller/UserController.java
@@ -4,7 +4,9 @@ import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +32,16 @@ public class UserController {
 		@GetMapping
 		public List<User> getAllUsers() {
 		    return userService.getAllUsers();
+		}
+		
+		@GetMapping("/{id}")
+		public User getUserById(@PathVariable Long id) {
+		    return userService.getUserById(id);
+		}
+
+		@DeleteMapping("/{id}")
+		public String deleteUser(@PathVariable Long id) {
+		    userService.deleteUser(id);
+		    return "User deleted successfully";
 		}
 }
