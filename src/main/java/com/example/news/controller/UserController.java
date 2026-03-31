065083819db50922ea006dad03e3d49e86a94412
Tlohitkumar <lohitkumarthotakura@gmail.com>
2026-03-31 17:56:16 +0530

    created a PutMapping controller through servive layer for API Testing

--- src/main/java/com/example/news/controller/UserController.java
+++ src/main/java/com/example/news/controller/UserController.java
@@ -2,12 +2,14 @@ package com.example.news.controller;
 
 import java.util.*;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,5 +45,10 @@ public class UserController {
 		public String deleteUser(@PathVariable Long id) {
 		    userService.deleteUser(id);
 		    return "User deleted successfully";
+		}
+		
+		@PutMapping("/{id}")
+		public User updateUser(@PathVariable Long id, @RequestBody User user) {
+		    return userService.updateUser(id, user);
 		}
 }
