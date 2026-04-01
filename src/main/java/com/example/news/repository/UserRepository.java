55565044c9d095bbb91881266d85ca86b8778872
Tlohitkumar <lohitkumarthotakura@gmail.com>
2026-04-01 15:00:09 +0530

    Added Optional which used to avoid null checks and prevent NullPointerException.”

--- src/main/java/com/example/news/repository/UserRepository.java
+++ src/main/java/com/example/news/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.example.news.repository;
 
 import com.example.news.model.User;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	 Optional<User> findByEmail(String email);
+	
 }
