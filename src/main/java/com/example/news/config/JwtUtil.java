0d78f0b54b14370e49ca9ad9f96b0a9763faa6bf
Tlohitkumar <lohitkumarthotakura@gmail.com>
2026-04-02 16:13:11 +0530

    Updated config

---
+++ src/main/java/com/example/news/config/JwtUtil.java
@@ -0,0 +1,27 @@
+package com.example.news.config;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    private String secret = "myverysecuresecretkeymyverysecuresecretkey123";
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256) // ✅ FIXED
+                .compact();
+    }
+}
