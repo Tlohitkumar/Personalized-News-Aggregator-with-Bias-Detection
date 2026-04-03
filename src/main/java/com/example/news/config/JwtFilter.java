9f3ab8d4670ba26be30ef982b131c0eda81998e4
Tlohitkumar <lohitkumarthotakura@gmail.com>
2026-04-03 16:31:20 +0530

    created a new file in config, Allowed login endpoint in JWT filter

---
+++ src/main/java/com/example/news/config/JwtFilter.java
@@ -0,0 +1,47 @@
+package com.example.news.config;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtFilter implements Filter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        
+        String path = req.getRequestURI();
+
+        if (path.contains("/login")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+
+        String header = req.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+
+            try {
+                jwtUtil.validateToken(token); // we will create this
+            } catch (Exception e) {
+                throw new ServletException("Invalid Token");
+            }
+        } else {
+            throw new ServletException("Missing Token");
+        }
+
+        chain.doFilter(request, response);
+    }
+}
