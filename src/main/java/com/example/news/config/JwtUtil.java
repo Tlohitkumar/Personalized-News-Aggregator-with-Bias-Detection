7871c970b0bdbe84d322d15ceff340114ae6e59a
Tlohitkumar <lohitkumarthotakura@gmail.com>
2026-04-05 16:31:46 +0530

    Added the role attributes to managae the Db

--- src/main/java/com/example/news/config/JwtUtil.java
+++ src/main/java/com/example/news/config/JwtUtil.java
@@ -16,9 +16,10 @@ public class JwtUtil {
         return Keys.hmacShaKeyFor(secret.getBytes());
     }
 
-    public String generateToken(String email) {
+    public String generateToken(String email, String role) {
         return Jwts.builder()
                 .setSubject(email)
+                .claim("role", role)   // ✅ add role
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256) // ✅ FIXED
@@ -30,5 +31,14 @@ public class JwtUtil {
             .setSigningKey(getSigningKey())
             .build()
             .parseClaimsJws(token);
+    }
+    
+    public String extractRole(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("role", String.class);
     }
 }
