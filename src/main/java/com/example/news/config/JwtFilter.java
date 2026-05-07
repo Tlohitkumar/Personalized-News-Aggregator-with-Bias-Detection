7c0cfa6aabf5dae643e8015543b8e2d1fd2fb502
Tlohitkumar <lohitkumarthotakura@gmail.com>
2026-05-07 09:42:04 +0530

    Upadted the jwtFilter code:

--- src/main/java/com/example/news/config/JwtFilter.java
+++ src/main/java/com/example/news/config/JwtFilter.java
@@ -1,76 +1,50 @@
 package com.example.news.config;
 
-import jakarta.servlet.*;
+import java.io.IOException;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
-public class JwtFilter implements Filter {
-
-    @Autowired
-    private JwtUtil jwtUtil;
+public class JwtFilter extends OncePerRequestFilter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
 
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse res = (HttpServletResponse) response;
-        
-     // ✅ CORS headers
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        res.setHeader("Access-Control-Allow-Headers", "*");
+        // Get API path
+        String path = request.getRequestURI();
 
-     // ✅ Allow OPTIONS (CORS preflight)
-        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
-            chain.doFilter(request, response);
-            return;
-        }
-        
-        String path = req.getRequestURI();
-        
-     
-        // ✅ Allow login & register
-        if (path.contains("/login") || path.contains("/register") || path.contains("/news") || path.contains("/favorites")) {
-            chain.doFilter(request, response);
+        // Public APIs (No Token Needed)
+        if (path.startsWith("/api/news")
+                || path.startsWith("/api/users")) {
+
+            filterChain.doFilter(request, response);
             return;
         }
 
-        String header = req.getHeader("Authorization");
+        // Protected APIs
+        String token = request.getHeader("Authorization");
 
-        if (header == null || !header.startsWith("Bearer ")) {
-        	 res.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
-             res.getWriter().write("Missing Token");
-             return;
-        }
+        // Missing Token
+        if (token == null || token.isEmpty()) {
 
-        String token = header.substring(7);
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
-        try {
-            jwtUtil.validateToken(token);
+            response.getWriter().write("Missing Token");
 
-            if ("DELETE".equalsIgnoreCase(req.getMethod())) {
-                String role = jwtUtil.extractRole(token);
-                if (!"ADMIN".equals(role)) {
-                	 res.setStatus(HttpServletResponse.SC_FORBIDDEN); // 403
-                     res.getWriter().write("Access Denied ❌");
-                     return;
-                }
-            }
-
-        } catch (Exception e) {
-        	res.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
-            res.getWriter().write("Invalid Token");
             return;
         }
 
-        chain.doFilter(request, response);
+        // Continue request
+        filterChain.doFilter(request, response);
     }
 }
